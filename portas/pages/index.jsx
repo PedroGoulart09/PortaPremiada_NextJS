b93c816d2ba809830222b4d4339837e270bc3e41
Pedro Goulart <ph.goulart.almeida@gmail.com>
2022-03-19 23:03:50 -0300

    start project

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -1,9 +1,15 @@
 import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
 
 export default function Home() {
+
+  const p1 = new PortaModel(1, false, true);
+  const p2 = new PortaModel(2);
+
   return (
-    <div>
-      <Porta />
+    <div style={{display: 'flex'}}>
+      <Porta porta={p1} />
+      <Porta porta={p2}/>
     </div>
   )
 }
